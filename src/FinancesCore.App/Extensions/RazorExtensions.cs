99faf0825409d3bcd856d2295cb3c6e546e963de
EvertonAlmeida <everton.o.almeida@outlook.com>
2021-05-29 09:03:06 -0300

    added Razor Extentions

---
+++ src/FinancesCore.App/Extensions/RazorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace FinancesCore.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string GetTypeDescription(this RazorPage page, int typeTransaction)
+        {
+            return typeTransaction == 1 ? "Incoming" : "Outcoming";
+        }
+    }
+}
