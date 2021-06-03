e8d6dacca20c01705b02619e7bf4a69b84037f6e
EvertonAlmeida <everton.o.almeida@outlook.com>
2021-06-03 15:30:05 -0300

    Feat: Added tagHelper to supress the html elements

---
+++ src/FinancesCore.App/Extensions/SupressElementTagHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing;
+
+namespace FinancesCore.App.Extensions
+{
+    [HtmlTargetElement("*", Attributes = "supress-by-claim-name")]
+    [HtmlTargetElement("*", Attributes = "supress-by-claim-value")]
+    public class SupressElementTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public SupressElementTagHelper(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        [HtmlAttributeName("supress-by-claim-name")]
+        public string IdentityClaimName { get; set; }
+
+        [HtmlAttributeName("supress-by-claim-value")]
+        public string IdentityClaimvalue { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null) throw new ArgumentException(nameof(context));
+            if (output == null) throw new ArgumentException(nameof(output));
+
+            var hasAccess = CustomAuthorization.ValidateUserClaims(_contextAccessor.HttpContext, IdentityClaimName, IdentityClaimvalue);
+
+            if (hasAccess) return;
+
+            output.SuppressOutput();
+        }
+    }
+
+    [HtmlTargetElement("*", Attributes = "supress-by-action")]
+    public class SupressElementByActionTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public SupressElementByActionTagHelper(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        [HtmlAttributeName("supress-by-action")]
+        public string ActionName { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null) throw new ArgumentException(nameof(context));
+            if (output == null) throw new ArgumentException(nameof(output));
+
+            var action = _contextAccessor.HttpContext.GetRouteData().Values["action"].ToString();
+
+            if (ActionName.Contains(action)) return;
+
+            output.SuppressOutput();
+        }
+    }
+}
