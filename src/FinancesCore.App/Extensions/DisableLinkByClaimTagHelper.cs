a15f5c2c0c0c68706ba13c9f1ef10b105576df70
EvertonAlmeida <everton.o.almeida@outlook.com>
2021-06-03 16:22:17 -0300

    Feat: Added tagHelper to disable the html elements

---
+++ src/FinancesCore.App/Extensions/DisableLinkByClaimTagHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace FinancesCore.App.Extensions
+{
+    [HtmlTargetElement("*", Attributes = "disable-by-claim-name")]
+    [HtmlTargetElement("*", Attributes = "disable-by-claim-value")]
+    public class DisableLinkByClaimTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public DisableLinkByClaimTagHelper(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        [HtmlAttributeName("disable-by-claim-name")]
+        public string IdentityClaimName { get; set; }
+
+        [HtmlAttributeName("disable-by-claim-value")]
+        public string IdentityClaimValue { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            var temAcesso = CustomAuthorization.ValidateUserClaims(_contextAccessor.HttpContext, IdentityClaimName, IdentityClaimValue);
+
+            if (temAcesso) return;
+
+            output.Attributes.RemoveAll("href");
+            output.Attributes.Add(new TagHelperAttribute("style", "cursor: not-allowed"));
+            output.Attributes.Add(new TagHelperAttribute("title", "You do not have permission."));
+        }
+    }
+}
